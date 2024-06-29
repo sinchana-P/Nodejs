1485866a22d2390b5bea73ed2d8ee6e383953e6b
Sinchana P <sinchana.p.swarna@gmail.com>
2024-06-30 02:22:48 +0530

    fs module 2

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-const fs = require('fs')
-fs.writeFileSync("hello.txt", "Hello World!")
-
-console.log("->>", __dirname);
-console.log("->>", __filename);
-
+const fs = require('fs').writeFileSync;
+fs("abc.txt", "abc")
