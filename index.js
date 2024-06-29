167f3d1390e54be651fc00427ef61f8b98869693
Sinchana P <sinchana.p.swarna@gmail.com>
2024-06-29 21:31:51 +0530

    js basics

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ var a = 20;
 var b = 30;
 var c = 40;
 
-console.warn(a+b+c)
+console.warn(a + b + c)
+
+// JS Basics
+var x = 20
+if(x===20) {
+    console.log("Matched")
+}
+
+for(i=0; i<10; i++){
+    console.log(i);
+}
+
+const arr=[2,4,7,1,3,8,3];
+console.log(arr);
+
