1f7c38584ee7991e68c9cdfd4280b0a527a3ab2d
Sinchana P <sinchana.p.swarna@gmail.com>
2024-06-30 11:31:51 +0530

    simple & basic api

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const colors = require('colors')
-console.log("file - package.json updated".blue)
+const http = require('http')
+
+http.createServer((req, resp) => {
+    resp.writeHead(200, { 'Content-Type': 'application\json' });
+    resp.write(JSON.stringify({ name: 'sinchana', role: 'developer' }));
+    resp.end()
+}).listen(4700);
