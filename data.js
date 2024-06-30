4807277118f5c495024ef060f1ce674e3d2ecff7
Sinchana P <sinchana.p.swarna@gmail.com>
2024-06-30 11:39:28 +0530

    export import data file

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    { name: 'Sinchana P Gudagi', role: 'developer' },
+    { name: 'Vaishnavi', role: 'doctor' },
+    { name: 'Zaiba', role: 'developer' }
+]
+
+module.exports = data;
